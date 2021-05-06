82beef39ee610f89124c58353afc464356782914
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-05-06 15:51:33 +0200

    add. route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/chi-siamo', function () {
+    return "<h1>chi siamo</h1>";
+});
